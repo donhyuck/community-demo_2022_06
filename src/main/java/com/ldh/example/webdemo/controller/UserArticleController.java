d6198075335ab937bb98e9e0977b2275df7b8bf0
donhyuck <leedonhyuck123@gmail.com>
2022-06-10 00:35:08 +0900

    컨트롤러-테스트테이터 생성 메서드 구현

--- src/main/java/com/ldh/example/webdemo/controller/UserArticleController.java
+++ src/main/java/com/ldh/example/webdemo/controller/UserArticleController.java
@@ -12,14 +12,34 @@ import com.ldh.example.webdemo.vo.Article;
 @Controller
 public class UserArticleController {
 
+	// 인스턴스 변수 시작
 	private int articleLastId;
 	private List<Article> articles;
+	// 인스턴스 변수 끝
 
+	// 생성자
 	public UserArticleController() {
 		articleLastId = 0;
 		articles = new ArrayList<>();
+
+		makeTestData();
 	}
 
+	// 서비스 메서드 시작
+	private void makeTestData() {
+		for (int i = 1; i <= 10; i++) {
+			int id = articleLastId + 1;
+			String title = "제목" + i;
+			String body = "내용" + i;
+			Article article = new Article(id, title, body);
+
+			articles.add(article);
+			articleLastId = id;
+		}
+	}
+	// 서비스 메서드 끝
+
+	// 액션 메서드 시작
 	@RequestMapping("/user/article/doAdd")
 	@ResponseBody
 	public Article doAdd(String title, String body) {
@@ -39,4 +59,5 @@ public class UserArticleController {
 
 		return articles;
 	}
+	// 액션 메서드 끝
 }
