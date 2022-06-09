c3042b02b0e1554f96d6f53f4fec654a9282032b
donhyuck <leedonhyuck123@gmail.com>
2022-06-10 00:38:37 +0900

    writeArticle메서드로 작성기능과 테스트데이터생성기능 중복코드 제거

--- src/main/java/com/ldh/example/webdemo/controller/UserArticleController.java
+++ src/main/java/com/ldh/example/webdemo/controller/UserArticleController.java
@@ -28,21 +28,14 @@ public class UserArticleController {
 	// 서비스 메서드 시작
 	private void makeTestData() {
 		for (int i = 1; i <= 10; i++) {
-			int id = articleLastId + 1;
 			String title = "제목" + i;
 			String body = "내용" + i;
-			Article article = new Article(id, title, body);
 
-			articles.add(article);
-			articleLastId = id;
+			writeArticle(title, body);
 		}
 	}
-	// 서비스 메서드 끝
 
-	// 액션 메서드 시작
-	@RequestMapping("/user/article/doAdd")
-	@ResponseBody
-	public Article doAdd(String title, String body) {
+	private Article writeArticle(String title, String body) {
 
 		int id = articleLastId + 1;
 		Article article = new Article(id, title, body);
@@ -50,6 +43,17 @@ public class UserArticleController {
 		articles.add(article);
 		articleLastId = id;
 
+		return article;
+	}
+
+	// 서비스 메서드 끝
+
+	// 액션 메서드 시작
+	@RequestMapping("/user/article/doAdd")
+	@ResponseBody
+	public Article doAdd(String title, String body) {
+
+		Article article = writeArticle(title, body);
 		return article;
 	}
 
