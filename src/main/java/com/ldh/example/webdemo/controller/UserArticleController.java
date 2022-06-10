77058195bca34c479e1f7e5fe8a64b8d31064067
donhyuck <leedonhyuck123@gmail.com>
2022-06-10 19:51:57 +0900

    컨트로러의 서비스 로직들을 ArticleService로 옮김

--- src/main/java/com/ldh/example/webdemo/controller/UserArticleController.java
+++ src/main/java/com/ldh/example/webdemo/controller/UserArticleController.java
@@ -14,77 +14,14 @@ import com.ldh.example.webdemo.vo.Article;
 @Controller
 public class UserArticleController {
 
-	// 인스턴스 변수 시작
 	@Autowired
 	ArticleService articleService;
-	
-	private int articleLastId;
-	private List<Article> articles;
-	// 인스턴스 변수 끝
 
-	// 생성자
-	public UserArticleController() {
-		articleLastId = 0;
-		articles = new ArrayList<>();
-
-		makeTestData();
-	}
-
-	// 서비스 메서드 시작
-	private void makeTestData() {
-		for (int i = 1; i <= 10; i++) {
-			String title = "제목" + i;
-			String body = "내용" + i;
-
-			writeArticle(title, body);
-		}
-	}
-
-	private Article writeArticle(String title, String body) {
-
-		int id = articleLastId + 1;
-		Article article = new Article(id, title, body);
-
-		articles.add(article);
-		articleLastId = id;
-
-		return article;
-	}
-
-	private Article getArticle(int id) {
-
-		for (Article article : articles) {
-			if (article.getId() == id) {
-				return article;
-			}
-		}
-
-		return null;
-	}
-
-	private void modifyArticle(int id, String title, String body) {
-
-		Article article = getArticle(id);
-
-		article.setTitle(title);
-		article.setBody(body);
-	}
-
-	private void deleteArticle(int id) {
-
-		Article article = getArticle(id);
-
-		articles.remove(article);
-	}
-
-	// 서비스 메서드 끝
-
-	// 액션 메서드 시작
 	@RequestMapping("/user/article/doAdd")
 	@ResponseBody
 	public Article doAdd(String title, String body) {
 
-		Article article = writeArticle(title, body);
+		Article article = articleService.writeArticle(title, body);
 		return article;
 	}
 
@@ -92,7 +29,7 @@ public class UserArticleController {
 	@ResponseBody
 	public Object getArticleForPrint(int id) {
 
-		Article article = getArticle(id);
+		Article article = articleService.getArticle(id);
 
 		return article;
 	}
@@ -101,20 +38,20 @@ public class UserArticleController {
 	@ResponseBody
 	public List<Article> getArticles() {
 
-		return articles;
+		return articleService.articles;
 	}
 
 	@RequestMapping("/user/article/doModify")
 	@ResponseBody
 	public String doModify(int id, String title, String body) {
 
-		Article article = getArticle(id);
+		Article article = articleService.getArticle(id);
 
 		if (article == null) {
 			return id + "번 게시물을 찾을 수 없습니다.";
 		}
 
-		modifyArticle(id, title, body);
+		articleService.modifyArticle(id, title, body);
 		return id + "번 게시물을 수정했습니다.";
 	}
 
@@ -122,14 +59,13 @@ public class UserArticleController {
 	@ResponseBody
 	public String doDelete(int id) {
 
-		Article article = getArticle(id);
+		Article article = articleService.getArticle(id);
 
 		if (article == null) {
 			return id + "번 게시물을 찾을 수 없습니다.";
 		}
 
-		deleteArticle(id);
+		articleService.deleteArticle(id);
 		return id + "번 게시물을 삭제했습니다.";
 	}
-	// 액션 메서드 끝
 }
--- src/main/java/com/ldh/example/webdemo/service/ArticleService.java
+++ src/main/java/com/ldh/example/webdemo/service/ArticleService.java
@@ -1,8 +1,69 @@
 package com.ldh.example.webdemo.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.ldh.example.webdemo.vo.Article;
 
 @Service
 public class ArticleService {
 
+	public int articleLastId;
+	public List<Article> articles;
+
+	public ArticleService() {
+		articleLastId = 0;
+		articles = new ArrayList<>();
+
+		makeTestData();
+	}
+
+	public void makeTestData() {
+		for (int i = 1; i <= 10; i++) {
+			String title = "제목" + i;
+			String body = "내용" + i;
+
+			writeArticle(title, body);
+		}
+	}
+
+	public Article writeArticle(String title, String body) {
+
+		int id = articleLastId + 1;
+		Article article = new Article(id, title, body);
+
+		articles.add(article);
+		articleLastId = id;
+
+		return article;
+	}
+
+	public Article getArticle(int id) {
+
+		for (Article article : articles) {
+			if (article.getId() == id) {
+				return article;
+			}
+		}
+
+		return null;
+	}
+
+	public void modifyArticle(int id, String title, String body) {
+
+		Article article = getArticle(id);
+
+		article.setTitle(title);
+		article.setBody(body);
+	}
+
+	public void deleteArticle(int id) {
+
+		Article article = getArticle(id);
+
+		articles.remove(article);
+	}
 }
