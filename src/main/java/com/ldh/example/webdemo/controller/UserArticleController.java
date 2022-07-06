f5f9e13b66ee30aad26cf462a4b274ff3721ed54
donhyuck <leedonhyuck123@gmail.com>
2022-07-06 13:34:02 +0900

    JSP구현 01/ 게시물 리스트를 JSP로 구현

--- src/main/java/com/ldh/example/webdemo/controller/UserArticleController.java
+++ src/main/java/com/ldh/example/webdemo/controller/UserArticleController.java
@@ -6,6 +6,7 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -33,13 +34,13 @@ public class UserArticleController {
 		return ResultData.from("S-1", Ut.f("%s번 게시물입니다.", id), "article", article);
 	}
 
-	@RequestMapping("/user/article/getArticles")
-	@ResponseBody
-	public ResultData<List<Article>> getArticles() {
+	@RequestMapping("/user/article/list")
+	public String showList(Model model) {
 
 		List<Article> articles = articleService.getArticles();
+		model.addAttribute("articles", articles);
 
-		return ResultData.from("S-1", "게시글 목록입니다.", "articles", articles);
+		return "user/article/list";
 	}
 
 	@RequestMapping("/user/article/doAdd")
