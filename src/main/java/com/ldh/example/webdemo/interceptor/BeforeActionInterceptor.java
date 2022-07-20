9d5fc3f574da6fb7f4070dca84705c06ebd5f60e
donhyuck <leedonhyuck123@gmail.com>
2022-07-20 21:09:26 +0900

    BeforeActionInterceptor 에서 Rq 객체를 좀 더 확실하게 생성되도록 유도

--- src/main/java/com/ldh/example/webdemo/interceptor/BeforeActionInterceptor.java
+++ src/main/java/com/ldh/example/webdemo/interceptor/BeforeActionInterceptor.java
@@ -3,21 +3,25 @@ package com.ldh.example.webdemo.interceptor;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
-import com.ldh.example.webdemo.service.MemberService;
+import com.ldh.example.webdemo.vo.Rq;
 
 @Component
 public class BeforeActionInterceptor implements HandlerInterceptor {
 
-	@Autowired
-	private MemberService memberService;
+	private Rq rq;
+
+	public BeforeActionInterceptor(Rq rq) {
+		this.rq = rq;
+	}
 
 	@Override
 	public boolean preHandle(HttpServletRequest req, HttpServletResponse resp, Object handler) throws Exception {
 
+		rq.initOnBeforeActionInterceptor();
+		
 		return HandlerInterceptor.super.preHandle(req, resp, handler);
 	}
 
