c466a13b3774cd65b124daacdf8966ac72c52815
donhyuck <leedonhyuck123@gmail.com>
2022-08-04 20:39:23 +0900

    인증코드 검사 01/ Attr서비스, Attr리포지터리 추가

---
+++ src/main/java/com/ldh/example/webdemo/vo/Attr.java
@@ -0,0 +1,22 @@
+package com.ldh.example.webdemo.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Attr {
+
+	private int id;
+	private String regDate;
+	private String updateDate;
+	private String expireDate;
+	private String relTypeCode;
+	private int relId;
+	private String typeCode;
+	private String type2Code;
+	private String value;
+
+}
